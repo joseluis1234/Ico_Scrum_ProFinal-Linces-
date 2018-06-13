bd77b0740ee16f5de03267acbd81c9e69dd78d91
joseluis1234 <mariobros1068@hotmail.com>
2018-06-12 23:17:53 -0500

    Se actualizara la carpeta

--- paginaLince/build/classes/fes/aragon/ico/fes/ventanaPrincipal.rs
+++
@@ -1,6 +0,0 @@
-fesa.ICO.paginaweb.ventanaPrincipal
-fesa.ICO.paginaweb.ventanaPrincipal$2
-fesa.ICO.paginaweb.ventanaPrincipal$1
-fesa.ICO.paginaweb.ventanaPrincipal$4
-fesa.ICO.paginaweb.ventanaPrincipal$3
-fesa.ICO.paginaweb.ventanaPrincipal$5
